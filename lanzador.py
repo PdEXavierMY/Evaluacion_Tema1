798d416529f685428ea958f3580c9a146e7a54fe
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-09-23 16:25:03 +0200

    nuevo main

--- lanzador.py
+++ lanzador.py
@@ -1,4 +1,5 @@
-from codigo.code import Matriz, Cadenatexto, Generadorlista, ScriptTabla
+from codigo.code import Matriz, Cadenatexto, Generadorlista, ScriptTabla, Codewars
+from introducir import solicitar_introducir_numero_extremo2
 
 def separador():
     print("--------------------------------------------------------")
@@ -15,3 +16,15 @@ def ejecutar():
     l1,l2,l3,l4,l5 = Generadorlista.generarlista()
     print(l1, l2, l3, l4, l5); separador()
     ScriptTabla.scripttabla()
+    separador()
+
+def ejecutar2():
+    eleccion = solicitar_introducir_numero_extremo2("¿Qué ejercicio quieres runnear?", 1, 5)
+    if eleccion == 3:
+        Generadorlista.generarnumero(0, [], 11, 1)
+        Generadorlista.generarnumero(-10, [], 1, 1)
+        Generadorlista.generarnumero(0, [], 21, 2)
+        Generadorlista.generarnumero(-19, [], 0, 2)
+        Generadorlista.generarnumero(0, [], 51, 5)
+    elif eleccion == 5:
+        Codewars.string_to_array(input("Introduce una cadena de texto: "))
