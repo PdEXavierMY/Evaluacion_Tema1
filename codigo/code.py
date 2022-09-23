7aab8081b0da14e67530fd239f2bd8831d027cce
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-09-23 12:19:03 +0200

    crear lanzador

--- codigo/code.py
+++ codigo/code.py
@@ -1,20 +1,20 @@
 class Matriz:
-    def __init__(self):
-        self.matriz = [[1,1,1,3],[2,2,2,7],[3,3,3,9],[4,4,4,13]]
+    def __init__(self, matriz):
+        self.matriz = matriz
+
+    matriz = [[1,1,1,3],[2,2,2,7],[3,3,3,9],[4,4,4,13]]
 
     def condicionmatriz(lista):
-        sum = 0
         for elemento in lista:
+            sum = 0
             for i in range(len(elemento)):
-                if i == 3:
-                    if elemento[i] == sum:
-                        print("Matriz correcta")
-                    else:
-                        elemento[i] = sum
-                        print("Matriz corregida")
-                        print(lista)
+                if i == 3 and elemento[i] != sum:
+                    elemento[i] = sum
+                    print("Matriz corregida")
+                    print(lista)
                 else:
                     sum+=int(elemento[i])
+        print("Matriz correcta")
         return lista
 
     def suma(lista):
@@ -23,6 +23,3 @@ class Matriz:
             for i in elemento:
                 sum+=int(i)
         return sum
-
-matriz = [[1,1,1,3],[2,2,2,7],[3,3,3,9],[4,4,4,13]]
-Matriz.condicionmatriz(matriz)
---
+++ codigo/lanzador.py
@@ -0,0 +1,5 @@
+from code import Matriz
+
+matriz = Matriz.matriz
+Matriz.condicionmatriz(matriz)
+suma = Matriz.suma(matriz); print(f"La función suma devuelve una suma de todos los elementos de la lista, que en este caso es igual a {suma}.")
